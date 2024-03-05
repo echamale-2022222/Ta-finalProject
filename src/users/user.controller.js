9915dfcafd23a07b4848f23762b91514f883e876
echamale-2022222 <echamale-2022222@kinal.edu.gt>
2024-03-05 05:53:41 -0600

    Bug

--- src/users/user.controller.js
+++ src/users/user.controller.js
@@ -2,21 +2,6 @@ import bycryptjs from "bcryptjs";
 import Admin from "../admin/admin.model.js"
 import Client from "../client/client.model.js"
 
-export const addAdmin = async (req = request, res = response) => {
-    const { name, mail, password } = req.body;
-    const admin = new Admin({ name, mail, password });
-
-    const salt = bycryptjs.genSaltSync();
-    admin.password = bycryptjs.hashSync(password, salt);
-
-    await admin.save();
-
-    res.status(200).json({
-        msg: "Administrator added to database",
-        admin
-    });
-}
-
 export const addClient = async (req = request, res = response) => {
     const { name, mail, password } = req.body;
     const client = new Client({ name, mail, password });
@@ -57,40 +42,6 @@ export const users = async (req, res) => {
         admins,
         clients
     });
-}
-
-export const updateRoleAdmin = async (req, res = response) => {
-    const { id } = req.params;
-    const {_id, name, mail, password, state, ...rest} = req.body;
-
-    await Admin.findByIdAndUpdate(id, rest);
-
-    let client;
-    if (rest.role === 'CLIENT') {
-        const existingClient = await Client.findById(id);
-        
-        if (existingClient) {
-            await Client.findByIdAndUpdate(id, { ...rest, state: true });
-        } else {
-            const updatedAdmin = await Admin.findById(id);
-            const client = new Client(updatedAdmin.toObject());
-            await client.save();
-        }
-
-        await Admin.findByIdAndUpdate(id, { state: false });
-
-        res.status(200).json({
-            msg: 'Updated admin to client',
-            client: client || existingClient
-        });
-    } else {
-        const updatedAdmin = await Admin.findOne({ _id: id });
-
-        res.status(200).json({
-            msg: 'Updated admin',
-            admin: updatedAdmin
-        });
-    }
 }
 
 export const updateRoleClient = async (req, res = response) => {
