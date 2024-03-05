63061f92a06c34e0421ad46c91bd63577459d9c6
echamale-2022222 <echamale-2022222@kinal.edu.gt>
2024-03-05 03:59:45 -0600

    Corrected category errors, and the creation of a new category to assign if it is deleted.

--- src/category/category.controller.js
+++ src/category/category.controller.js
@@ -1,26 +1,36 @@
 import { request, response } from "express";
 import Category from "./category.model.js";
+import Product from "../product/product.model.js";
 
 export const categoryPost = async (req = request, res = response) => {
-    const { categoryName, categoryDescription } = req.body;
-    const category = new Category({ categoryName, categoryDescription });
+    const admin = req.admin.role;
 
-    await category.save();
+    if (admin !== 'ADMIN') {
+        return res.status(401).json({
+            msg: "You don't have the necessary permissions"
+        });
+    } else {
+        const { categoryName, categoryDescription } = req.body;
+        const category = new Category({ categoryName, categoryDescription });
 
-    res.status(200).json({
-        msg: "Category added to the database",
-        category
-    });
+        await category.save();
+
+        res.status(200).json({
+            msg: "Category added to the database",
+            category
+        });
+    }
+
 }
 
 export const existingCategories = async (req, res) => {
-    const query = {statusCategory: true};
+    const query = { statusCategory: true };
 
     const [quantityCategories, categories] = await Promise.all([
         Category.countDocuments(query),
         Category.find(query)
     ]);
-    
+
     res.status(200).json({
         msg: "The existing categories",
         quantityCategories,
@@ -30,26 +40,52 @@ export const existingCategories = async (req, res) => {
 
 export const updateCategory = async (req, res = response) => {
     const { id } = req.params;
-    const {_id, ...rest} = req.body;
+    const { _id, ...rest } = req.body;
+    const admin = req.admin.role;
 
-    await Category.findByIdAndUpdate(id, rest);
+    if (admin !== 'ADMIN') {
+        return res.status(401).json({
+            msg: "You don't have the necessary permissions"
+        });
+    } else {
+        await Category.findByIdAndUpdate(id, rest);
+        const category = await Category.findOne({ _id: id });
 
-    const category = await Category.findOne({_id: id});
-
-    res.status(200).json({
-        msg: 'Updated category',
-        category
-    });
+        res.status(200).json({
+            msg: 'Updated category',
+            category
+        });
+    }
 }
 
 export const deleteCategory = async (req, res) => {
-    const {id} = req.params;
+    const { id } = req.params;
+    const admin = req.admin.role;
 
-    const category = await Category.findByIdAndUpdate(id, {statusCategory: false});
-    const categoryDel = await Category.findOne({_id: id});
+    if (admin !== 'ADMIN') {
+        return res.status(401).json({
+            msg: "You don't have the necessary permissions"
+        });
+    } else {
+        let alternativeCategory;
 
-    res.status(200).json({
-        msg: "Deleted category",
-        categoryDel
-    });
+        alternativeCategory = await Category.findOne({ categoryName: "Not assigned" });
+
+        if (!alternativeCategory) {
+            alternativeCategory = await Category.create({
+                categoryName: "Not assigned",
+                categoryDescription: "This category is assigned to products that do not have a category"
+            });
+        }
+
+        await Product.updateMany({ productCategory: id }, { productCategory: alternativeCategory._id });
+
+        await Category.findByIdAndUpdate(id, { statusCategory: false });
+        const category = await Category.findById(id);
+
+        res.status(200).json({
+            msg: "Deleted category",
+            category
+        });
+    }
 }
--- src/category/category.routes.js
+++ src/category/category.routes.js
@@ -1,14 +1,18 @@
 import { Router } from "express";
 import { check } from "express-validator";
 import { categoryPost, deleteCategory, existingCategories, updateCategory } from "./category.controller.js";
+import { validateFields }  from "../middlewares/validate-fields.js";
+import { validateJWT } from "../middlewares/validate-jwt.js";
 
 const router = Router();
 
 router.post(
     "/",
     [
+        validateJWT,
         check("categoryName", "The category name cannot be empty").not().isEmpty(),
         check("categoryDescription", "The category description cannot be empty").not().isEmpty(),
+        validateFields,
     ], categoryPost)
 
 router.get("/", existingCategories);
@@ -16,12 +20,15 @@ router.get("/", existingCategories);
 router.put(
     "/:id",
     [
+        validateJWT,
         check("id", "It is not a valid id").isMongoId(),
+        validateFields,
     ], updateCategory);
 
 router.delete(
     "/:id",
     [
+        validateJWT,
         check("id", "It is not a valid id").isMongoId(),
     ], deleteCategory)
 
