1533dae35c846c490905187a9e11b379ef437225
echamale-2022222 <echamale-2022222@kinal.edu.gt>
2024-03-06 17:11:31 -0600

    add change2

--- src/shoppingCart/shoppingCart.controller.js
+++ src/shoppingCart/shoppingCart.controller.js
@@ -1,24 +1,39 @@
 import { request, response } from "express";
-import ShoppingCart from "./shoppingCart.model.js";
+import Client from "../client/client.model.js";
+import Product from "../product/product.model.js";
+
+let shoppingCart = [];
 
 export const createShoppingCart = async (req = request, res = response) => {
-    try {
-        const cliente =  req.client._id;
-        const { id } = req.params;
-        const { quantity } = req.body;
+    const cliente = req.client._id;
+    const { productName, quantity } = req.body;
 
-        const shoppingCart = new ShoppingCart({ client: cliente, products: [{ productId: id, quantity }]});
+    const client = await Client.findOne({ _id: cliente });
+    const product = await Product.findOne({ productName: productName });
 
-        await shoppingCart.save();
-
-        res.status(200).json({
-            message: "Shopping cart created",
-            shoppingCart
-        });
-    } catch (error) {
-        console.log(error),
-        res.status(500).json({
-            message: "Error creating shopping cart",
+    if (!client) {
+        return res.status(404).json({
+            message: "Client not found",
         });
     }
+    if (!product) {
+        return res.status(404).json({
+            message: "Product not found",
+        });
+    } else if (product.productStock < quantity) {
+        return res.status(400).json({
+            message: "There is not enough stock",
+        });
+    }
+
+    const total = product.productPrice * quantity;
+
+    shoppingCart.push(
+        cliente, product._id, quantity, total
+    );
+
+    res.status(201).json({
+        message: "Product added to shopping cart",
+        shoppingCart
+    });
 }
--- src/shoppingCart/shoppingCart.routes.js
+++ src/shoppingCart/shoppingCart.routes.js
@@ -6,10 +6,11 @@ import { validateClientJWT } from "../middlewares/validate-jwt.js";
 const router = Router();
 
 router.post(
-    "/add/:id",
+    "/add",
     [
         validateClientJWT,
         check("id", "The id is required").not().isEmpty(),
+        check("productName", "The product name is required").not().isEmpty(),
         check("quantity", "The quantity is required").not().isEmpty(),
     ], createShoppingCart);
 
